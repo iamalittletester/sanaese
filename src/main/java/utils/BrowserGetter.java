36d60f6ea3b5103227c00dc7b17eecf733596ac3
iamalittletester <capreoara6@gmail.com>
2023-10-19 22:06:25 +0300

    Initial commit.

---
+++ src/main/java/utils/BrowserGetter.java
@@ -0,0 +1,13 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserGetter {
+
+    public WebDriver getDriver() {
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize();
+        return chromeDriver;
+    }
+}
